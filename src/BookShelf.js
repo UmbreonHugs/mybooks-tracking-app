1056214200a8d5ac135198a43d560edf46a46c17
Luigi <thegaminggodlou@gmail.com>
2018-10-10 17:50:50 -0700

    Added files, and seperated templates into their own files, added router

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -2,4 +2,39 @@
 *   This component will render the book shelf, which will render the individual books
 *   via ListBooks.JS
 */
-import React from 'react'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+class BookShelf extends Component {
+  static propTypes = {
+    books: PropTypes.array.isRequired
+  }
+  render() {
+    return (
+      <div>
+        <div className="bookshelf">
+          <h2 className="bookshelf-title">Currently Reading</h2>
+          <div className="bookshelf-books">
+            <ol className="books-grid">
+            </ol>
+          </div>
+        </div>
+        <div className="bookshelf">
+          <h2 className="bookshelf-title">Want to Read</h2>
+          <div className="bookshelf-books">
+            <ol className="books-grid">
+            </ol>
+          </div>
+        </div>
+        <div className="bookshelf">
+          <h2 className="bookshelf-title">Read</h2>
+          <div className="bookshelf-books">
+            <ol className="books-grid">
+            </ol>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+export default BookShelf
